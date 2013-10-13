438661aae499b8f7301e4d24f08612c50d80373a
Todd Patrick <todd.patrick@espn.com>
2013-10-13 13:33:30 -0400

    Initial commit.

---
+++ src/com/todd/geoquiz/MainActivity.java
@@ -0,0 +1,49 @@
+package com.todd.geoquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	
+	private Button mTrueButton;
+	private Button mFalseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        this.mTrueButton = (Button)findViewById(R.id.true_button);
+        this.mFalseButton = (Button)findViewById(R.id.false_button);
+        
+        this.mTrueButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+        
+        this.mFalseButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
